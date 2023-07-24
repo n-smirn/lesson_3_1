aa5944ba9b80b34fb4837baa58506d4772026a0a
n-smirn <smirnovaswan@gmail.com>
2023-07-25 01:32:59 +0500

    first commit

---
+++ src/test/java/SearchGoogle.java
@@ -0,0 +1,23 @@
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+        import static com.codeborne.selenide.Condition.text;
+        import static com.codeborne.selenide.Selenide.$;
+        import static com.codeborne.selenide.Selenide.open;
+
+ public class SearchGoogle {
+        @BeforeAll
+        static void beforeAll() {
+        Configuration.browser = "chrome";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1920x1080";
+        }
+        @Test
+        void searchTest () {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://ru.selenide.org"));
+        }
+        }
